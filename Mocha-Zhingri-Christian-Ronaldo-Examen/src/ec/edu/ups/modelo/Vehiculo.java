3c1848cc057f6468f1f6b377ae36a515d4c389ef
ChristianMocha <34308608+ChristianMocha@users.noreply.github.com>
2020-06-02 10:01:12 -0500

    Primer commit

---
+++ Mocha-Zhingri-Christian-Ronaldo-Examen/src/ec/edu/ups/modelo/Vehiculo.java
@@ -0,0 +1,124 @@
+package ec.edu.ups.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+public class Vehiculo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int id;
+	private String placa;
+	private String marca;
+	private String modelo;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn
+	private Cliente cliente;
+	public Vehiculo() {
+		super();
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getPlaca() {
+		return placa;
+	}
+
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vehiculo other = (Vehiculo) obj;
+		if (id != other.id)
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		if (placa == null) {
+			if (other.placa != null)
+				return false;
+		} else if (!placa.equals(other.placa))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo [id=" + id + ", placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + "]";
+	}
+	
+	
+
+}
